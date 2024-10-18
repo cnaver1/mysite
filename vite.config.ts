4d17f5bff74b50c96ec3cd26b696c77ccbd65394
Cameron <cameron.navero@yahoo.com>
2024-10-17 20:47:28 -0500

    setup docker

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const viteConfig =  defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allow access from outside the container
+    strictPort:true,
+    port: 8080, // Specify the port
+    watch: {
+      usePolling: true,
+    },
+  },
 })
+
+
+export default viteConfig
